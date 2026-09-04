0dd0045554e75bdcebb8f05e9ae8852420a02fb1
mentooor <artur.alekseenko.2002@gmail.com>
2026-09-04 16:31:48 +0200

    add data to charts

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
       <div className="flex items-center gap-2.5 px-5 py-5">
 <img
 
-  src="/elfduck-logo.png"
+  src="/public/elfduck-logo.png"
 
   alt="ElfDuck"
 
